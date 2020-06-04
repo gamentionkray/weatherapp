41fc8505f328faebe1ceff8101af3fc7c29ce8fa
Krishna Thorat <krishnathorat007@gmail.com>
2020-06-04 11:31:41 +0530

    Updates

---
+++ precache-manifest.69d879a814d00c73553ad20fdb936b98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f507b2a9e222af7d28d331d33accc7",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "fe6c75cb64a22ba7d2c0",
+    "url": "/weatherapp/static/css/main.84681c56.chunk.css"
+  },
+  {
+    "revision": "26a9671610cda846a97e",
+    "url": "/weatherapp/static/js/2.82bd4be9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.82bd4be9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe6c75cb64a22ba7d2c0",
+    "url": "/weatherapp/static/js/main.7daacf92.chunk.js"
+  },
+  {
+    "revision": "8ed7e94a2cf8b1969762",
+    "url": "/weatherapp/static/js/runtime-main.d767ab78.js"
+  }
+]);
